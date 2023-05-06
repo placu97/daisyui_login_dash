9e7075d09c4fafb9e40830db0c8f4b6c9a0e0a00
placu97 <matteogplacucci@gmail.com>
2023-05-06 22:13:20 +0200

    autosave

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen bg-pink-500">
+      <h1 className="text-6xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
